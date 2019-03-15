0a58cde118217764956e2ba0d2598e19ec13548b
JUAN DAVID DIAZ CRISTANCHO <jd.diazc@sis.virtual.uniandes.edu.co>
2019-03-15 16:50:54 -0500

    Cómete tu mierda

---
+++ src/negocio/VOHotel.java
@@ -0,0 +1,15 @@
+package negocio;
+
+/**
+ * Interfaz para los metodos get de HOTEL
+ * @author jd.diazc
+ *
+ */
+public interface VOHotel {
+
+	/**
+	 * 
+	 * @return las estrellas de un hotel
+	 */
+	public Integer getEstrellas();
+}
---
+++ src/negocio/VOInternet.java
@@ -0,0 +1,15 @@
+package negocio;
+
+/**
+ * Interfaz para los metodos get de INTERNET
+ * @author jd.diazc
+ *
+ */
+public interface VOInternet {
+
+	public Double getCapacidad();
+	
+	public Double getCosto();
+	
+	public String getDescripcion();
+}
