e82f1c378fa8b3e6cc123ac39c476419786ab3d0
Pedro Londoño <pedrolondono@Pedros-iMac.local>
2019-03-16 20:07:14 -0500

    MERA BABY DILE LA VERDAD

--- src/persistencia/SQLUtil.java
+++ src/persistencia/SQLUtil.java
@@ -10,7 +10,7 @@ class SQLUtil {
 	 *****************************************************************/
 	/**
 	 * Cadena que representa el tipo de consulta que se va a realizar en las sentencias de acceso a la base de datos
-	 * Se renombra acá para facilitar la escritura de las sentencias
+	 * Se renombra acï¿½ para facilitar la escritura de las sentencias
 	 */
 	private final static String SQL = PersistenciaCadenaHoteles.SQL;
 	
@@ -18,17 +18,17 @@ class SQLUtil {
 	 * 			Atributos
 	 *****************************************************************/
 	/**
-	 * El manejador de persistencia general de la aplicación
+	 * El manejador de persistencia general de la aplicaciï¿½n
 	 */
 	private PersistenciaCadenaHoteles pp;
 	
 	/* ****************************************************************
-	 * 			Métodos
+	 * 			Mï¿½todos
 	 *****************************************************************/
 	
 	/**
 	 * Constructor
-	 * @param pp - El Manejador de persistencia de la aplicación
+	 * @param pp - El Manejador de persistencia de la aplicaciï¿½n
 	 */
 	public SQLUtil (PersistenciaCadenaHoteles pp)
 	{
@@ -36,9 +36,9 @@ class SQLUtil {
 	}
 	
 	/**
-	 * Crea y ejecuta la sentencia SQL para obtener un nuevo número de secuencia
+	 * Crea y ejecuta la sentencia SQL para obtener un nuevo nï¿½mero de secuencia
 	 * @param pm - El manejador de persistencia
-	 * @return El número de secuencia generado
+	 * @return El nï¿½mero de secuencia generado
 	 */
 	public long nextval (PersistenceManager pm)
 	{
@@ -51,11 +51,44 @@ class SQLUtil {
 	/**
 	 * Crea y ejecuta las sentencias SQL para cada tabla de la base de datos - EL ORDEN ES IMPORTANTE 
 	 * @param pm - El manejador de persistencia
-	 * @return Un arreglo con 7 números que indican el número de tuplas borradas en las tablas 
+	 * @return Un arreglo con 7 nï¿½meros que indican el nï¿½mero de tuplas borradas en las tablas 
 	 */
 	public long [] limpiarCadenaHoteles (PersistenceManager pm)
 	{
-		return null;
+		Query qClientes = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaClientes());
+		Query qEmpleados = pm.newQuery(SQL, "DELETE FROM "+pp.darTablaEmpleados());
+		Query qGastos = pm.newQuery(SQL,"DELETE FROM"+pp.darTablaGastos());
+		Query qHabitaciones = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaHabitaciones());
+		Query qHoteles = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaHoteles());
+		Query qPlanesDeConsumo = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaPlanesDeConsumo());
+		Query qProductos = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaProductos());
+		Query qReservasHabitaciones = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaReservasHabitaciones());
+		Query qReservasServicios = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaReservasServicios());
+		Query qServicios = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaServicios());
+		Query qTipoIdentificacion = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaTipoIdentificacion());
+		Query qTipoPlanDeConsumo=pm.newQuery(SQL,"DELETE FROM "+pp.darTablaTipoPlanDeConsumo());
+		Query qTipoReservaHabitacion=pm.newQuery(SQL,"DELETE FROM "+pp.darTablaTipoReservaHabitacion());
+		Query qTipoRol = pm.newQuery(SQL,"DELETE FROM "+pp.darTablaTipoRol());
+		
+		long clientesEliminados = (long) qClientes.executeUnique();
+		long empleadosEliminados = (long) qEmpleados.executeUnique();
+		long gastosEliminados = (long) qGastos.executeUnique();
+		long habitacionesEliminados = (long) qHabitaciones.executeUnique();
+		long hotelesEliminados = (long) qHoteles.executeUnique();
+		long planesDeConsumoEliminados = (long)  qPlanesDeConsumo.executeUnique();
+		long productosEliminados = (long) qProductos.executeUnique();
+		long reservasHabitacionesEliminados = (long) qReservasHabitaciones.executeUnique();
+		long reservasServiciosEliminados = (long) qReservasServicios.executeUnique();
+		long serviciosEliminados = (long) qServicios.executeUnique();
+		long tipoIdentificacionEliminados = (long) qTipoIdentificacion.executeUnique();
+		long tipoPlanDeConsumoEliminados = (long) qTipoPlanDeConsumo.executeUnique();
+		long tipoReservaHabitacionEliminados = (long) qTipoReservaHabitacion.executeUnique();
+		long tipoRolEliminados = (long) qTipoRol.executeUnique();
+		
+		return new long[]{clientesEliminados,empleadosEliminados,gastosEliminados,habitacionesEliminados,hotelesEliminados,
+		                planesDeConsumoEliminados, productosEliminados, reservasHabitacionesEliminados, reservasServiciosEliminados,
+		                serviciosEliminados,tipoIdentificacionEliminados,tipoPlanDeConsumoEliminados,tipoReservaHabitacionEliminados,
+		                tipoRolEliminados};
 	}
 	
 }
